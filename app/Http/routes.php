ac5ff03614b96aaf600c5a9f8461310c1923295e
Francisco José de las Cabadas Balbuena <francisco.cabadas95@gmail.com>
2020-03-04 22:38:24 +0100

    añadiendo mas cosas del Route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,17 +15,39 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::pattern('hex', '[a-z0-9]+' );           //declara que solo podrá tener en la url caracteres hexadecimales abajo vemos como se usa
 
-Route::get('welcome', function () {                   //ejemplo de ruta
+Route::get('welcome/{hex?}', function ($hex = null) {                   //ejemplo de ruta (no funciona por que hay otro metodo igual con any
     return "hola mundo";
 });
 
+Route::pattern('num', '\d+');                 //solo permite numeros, se utiliza abajo
+
+Route::get('numero/{num?}', function ($num = null) {       //al poner esta url solo puedo poner numeros
+    return "hola mundo $num";
+});
+
+
+
+Route::get('post/id/{id}/nombre/{nombre}', function ($id, $nombre) {                   //ejemplo de ruta con parametros
+    return "hola mundo  $id  $nombre";
+});
+
+Route::get('post/id/{id}/nombre/{nombre?}', function ($id, $nombre = null) {                   //ejemplo de ruta con parametros optativos
+    if($nombre == null){
+        return "id no especificado, mostrariamos todos los ids, (por ejemplo)";
+    }
+    return "hola mundo  $id  $nombre";
+});
 
 Route::any('welcome', function () {                  //cualquien peticion http(post, get, delete, etc) la procesa con any
     $url = url("welcome");
     return "estamos en $url";
 
 });
+
+
+
 
 //Route de forma individual para distintas peticiones Http
 
